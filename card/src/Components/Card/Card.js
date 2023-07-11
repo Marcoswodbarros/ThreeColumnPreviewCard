2c797e776b4bf7dd6432eb5e37be294e88e6afa2
Marcos <marcoswodbarros@outlook.com>
2023-07-10 22:34:54 -0300

    Mobile: Creating card component

--- card/src/Components/Card/Card.js
+++ card/src/Components/Card/Card.js
@@ -5,7 +5,9 @@ const Card = () => {
     return (
         <div>
             <div className="card__container">
-                
+                <img src="/images/icon-sedans.svg" alt="Icon Sedan" />
+                <h1>Sedans</h1>
+                <p className='card__text'>Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip.</p>
             </div>
         </div>
     )
