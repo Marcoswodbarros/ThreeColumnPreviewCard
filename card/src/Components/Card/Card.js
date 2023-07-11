b575d4f198957e6d2710256fdd4e3731b7b3e5e8
Marcos <marcoswodbarros@outlook.com>
2023-07-10 22:47:09 -0300

    Mobile: Finishing components

--- card/src/Components/Card/Card.js
+++ card/src/Components/Card/Card.js
@@ -6,7 +6,7 @@ const Card = () => {
     return (
         <div>
             <div className="card__container">
-                <img src="/images/icon-sedans.svg" alt="Icon Sedan" />
+                <img src="/images/icon-sedans.svg" className='card__img' alt="Icon Sedan" />
                 <h1>Sedans</h1>
                 <p className='card__text'>Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip.</p>
                 <Button />
