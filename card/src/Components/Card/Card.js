8b42a3d7f2172e639bfba50e76fb753141321575
Marcos <marcoswodbarros@outlook.com>
2023-07-10 22:43:29 -0300

    Mobile: Creating button component

--- card/src/Components/Card/Card.js
+++ card/src/Components/Card/Card.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Card.css';
+import Button from '../Button/Button';
 
 const Card = () => {
     return (
@@ -8,6 +9,7 @@ const Card = () => {
                 <img src="/images/icon-sedans.svg" alt="Icon Sedan" />
                 <h1>Sedans</h1>
                 <p className='card__text'>Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip.</p>
+                <Button />
             </div>
         </div>
     )
