56fe6b7b648b79a2cdbd24d1142e43ab5cabefc7
Marcos <marcoswodbarros@outlook.com>
2023-07-11 10:41:25 -0300

    Mobile: Updating h1 props

--- card/src/App.js
+++ card/src/App.js
@@ -3,29 +3,12 @@ import Card from './Components/Card/Card';
 
 function App() {
 
-  const espec = [
-    {
-      h1: 'Sedan',
-      background: 'hsl(31, 77%, 52%)'
-    },
-
-    {
-      h1: 'Suvs',
-      background: 'hsl(184, 100%, 22%)'
-    },
-
-    {
-      h1: 'Luxury',
-      background: 'hsl(179, 100%, 13%)'
-    }
-  ]
-
   return (
     <div className="App">
       <header className="App-header">
-        <Card />
-        <Card />
-        <Card />
+        <Card nome='Sedans' />
+        <Card nome='Suv' />
+        <Card nome='Luxury' />
       </header>
     </div>
   );
--- card/src/Components/Card/Card.js
+++ card/src/Components/Card/Card.js
@@ -2,12 +2,12 @@ import React from 'react';
 import './Card.css';
 import Button from '../Button/Button';
 
-const Card = () => {
+const Card = (props) => {
     return (
         <div>
             <div className="card__container">
                 <img src="/images/icon-sedans.svg" className='card__img' alt="Icon Sedan" />
-                <h1>Sedans</h1>
+                <h1>{props.nome}</h1>
                 <p className='card__text'>Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip.</p>
                 <Button />
             </div>
