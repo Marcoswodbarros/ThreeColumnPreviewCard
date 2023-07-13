7a75f759604c8def2c36665d557332cbc1ca7046
Marcos <marcoswodbarros@outlook.com>
2023-07-13 16:43:51 -0300

    Mobile: Creating list iteration

--- card/src/App.js
+++ card/src/App.js
@@ -6,9 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Card nome='Sedans' img='/images/icon-sedans.svg' />
-        <Card nome='Suv' img='/images/icon-suvs.svg' />
-        <Card nome='Luxury' img='/images/icon-luxury.svg' />
+        <Card />
       </header>
     </div>
   );
--- card/src/Components/Card/Card.js
+++ card/src/Components/Card/Card.js
@@ -2,16 +2,42 @@ import React from 'react';
 import './Card.css';
 import Button from '../Button/Button';
 
-const Card = (props) => {
+const Card = () => {
+
+    const textos = [
+        {
+            image: './images/icon-sedans.svg',
+            nome: 'Sedans',
+            texto: 'Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip.'
+        },
+
+        {
+            image: './images/icon-suvs.svg',
+            nome: 'Suv',
+            texto: 'Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacation and off-road adventures.'
+        },
+
+        {
+            image: './images/icon-luxury.svg',
+            nome: 'Luxury',
+            texto: 'Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury rental and arrive in style.'
+        }
+    ]
+
     return (
         <div>
-            <div className="card__container">
-                <img src={props.img} className='card__img' alt="Icon Sedan" />
-                <h1>{props.nome}</h1>
-                <p className='card__text'>Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip.</p>
-                <Button />
-            </div>
-        </div>
+            {textos.map((item) => {
+                return (
+                    <div>
+                        <img src={item.image} alt="Logo Sedan" />
+                        <h1>{item.nome}</h1>
+                        <p>{item.texto}</p>
+                    </div>
+                )
+            })}
+
+            <Button />
+        </div >
     )
 }
 
