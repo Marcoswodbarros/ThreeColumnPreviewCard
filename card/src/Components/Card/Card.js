5294a5eef8a83d221da8eec89dbebbef10502578
Marcos <marcoswodbarros@outlook.com>
2023-07-18 23:52:20 -0300

    Mobile: Creating bg items

--- card/src/Components/Card/Card.js
+++ card/src/Components/Card/Card.js
@@ -6,18 +6,21 @@ const Card = () => {
 
     const textos = [
         {
+            bg: 'hsl(31, 77%, 52%)',
             image: './images/icon-sedans.svg',
             nome: 'Sedans',
             texto: 'Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip.'
         },
 
         {
+            bg: 'hsl(184, 100%, 22%)',
             image: './images/icon-suvs.svg',
             nome: 'Suv',
             texto: 'Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacation and off-road adventures.'
         },
 
         {
+            bg: 'hsl(179, 100%, 13%)',
             image: './images/icon-luxury.svg',
             nome: 'Luxury',
             texto: 'Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury rental and arrive in style.'
@@ -28,7 +31,7 @@ const Card = () => {
         <div>
             {textos.map((item) => {
                 return (
-                    <div key={item.nome} className='card__container'>
+                    <div key={item.nome} className='card__container' style={{background: item.bg}}>
                         <img src={item.image} className='card__img' alt="Logo Sedan" />
                         <h1>{item.nome}</h1>
                         <p>{item.texto}</p>
